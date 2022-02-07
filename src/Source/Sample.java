f10efb62ebe398d7e57f5e46cf86236378d35b5b
RanjithPugazhendhi <ranjithpugazhendhi1995@gmail.com>
2022-02-07 16:42:39 +0530

    hello

---
+++ src/Source/Sample.java
@@ -0,0 +1,24 @@
+package Source;
+
+public class Sample {
+		private void run() {
+	  
+			
+			System.out.println("petrol");
+			
+		}
+		
+	public static void main(String[] args) {
+		Sample a=new Sample();	
+		a.run();
+	}
+	}
+		
+		
+		
+		
+		
+		
+		
+
+
